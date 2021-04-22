f7ada4fb4649452c9f3e005b9e4982942f2f865c
sparktwinny <spark@twinny.co.kr>
2021-04-22 12:26:09 +0900

    cmt

--- src/lib/devOpsUtil.js
+++ src/lib/devOpsUtil.js
@@ -1,4 +1,8 @@
-
+var AWS = require("aws-sdk");
+const axios = require('axios').default;
+AWS.config.update({
+    region: "ap-northeast-2"
+});
 const yaml = require('js-yaml');
 const fs = require('fs');
 const fspr = require('fs').promises;
@@ -302,6 +306,317 @@ async function printServerlessFunction(stage, templateFile, apiSpecList) {
     fs.writeFileSync(`serverless.yml`, yamlStr, 'utf8');
 }
 
+async function handleCommit(stackname, stage, version, title, repoName, branch, confluenceSpaceName, confluencePageId, ancestorsPageId, confluenceUserId, confluencePassword) {
+
+    console.log("stackname", stackname);
+
+    var cloudformation = new AWS.CloudFormation();
+
+    var params = {
+
+        StackName: stackname
+    };
+    //const stack=await  cloudformation.describeStacks(params).promise();
+
+    const stack = await cloudformation.describeStacks(params).promise();
+
+    const info = getOutputs(stack);
+
+
+
+    var params = {
+        commitSpecifier: branch,
+        folderPath: 'src/lambda', /* required */
+        repositoryName: repoName, /* required */
+    };
+    let files = await getFileListFromLocal("./src/lambda", []);
+    const apiSpecList = await getApiSepcList(files);
+
+
+
+    await updateDocument(info.ServiceEndpoint, stage, version, title, apiSpecList, confluenceSpaceName, confluencePageId, ancestorsPageId)
+}
+async function updateDocument(host, stage, version, title, apiSpecList, confluenceSpaceName, confluencePageId, ancestorsPageId, confluenceUserId, confluencePassword) {
+    let cnt = 0;
+    //HTML 작성
+    let html = "";
+    html += `<br></br>`;
+    html += `<p><b>수정시간</b>:${moment().tz("Asia/Seoul").format("YYYY-MM-DD HH:mm:ss")}</p>`;
+    html += `<p><b>버전</b>:${stage}-${version}</p>`;
+    html += `<p><b>host</b>:${host}</p>`;
+    let errorTitles = [];
+    let errorValues = [];
+    let apiName = [];
+    for (var property in apiSpecList) {
+        let htmlTable = "";
+        let apiCount = 0;
+        let apiSpec = apiSpecList[property];
+        if (apiSpec.length < 1) {
+
+        } else {
+            cnt = 0;
+            htmlTable += `<br></br>`;
+            htmlTable += `<p>category:${property}</p>`;
+            htmlTable += `<table class="wrapped relative-table" >`;
+            htmlTable += `
+            <colgroup> 
+            <col style="width: 3.09584%;"/>
+            <col style="width: 5.72519%;"/> 
+            <col style="width: 6.70059%;"/> 
+            <col style="width: 7.88804%;"/> 
+            <col style="width: 3.54877%;"/> 
+            <col style="width: 10.8363%;"/> 
+            <col style="width: 20.2061%;"/> 
+            <col style="width: 20.3478%;"/>
+            <col style="width: 3.6514%;"/> 
+            <col style="width: 8.6514%;"/> 
+            <col style="width: 8.6514%;"/> 
+            </colgroup>`;
+
+            htmlTable += `<tbody>`;
+
+
+            htmlTable += `<tr><td style="text-align: center;">
+            <p align="center">번호</p>
+            </td>
+            <td colspan="1" style="text-align: center;">
+            <p align="center">종류</p> 
+            </td>
+            <td style="text-align: center;">
+            <p align="center">명칭</p>
+            </td>
+            <td colspan="1" style="text-align: center;">
+            <p align="center">API_URI</p>            
+            </td>
+            <td colspan="1" style="text-align: center;">
+            <p align="center">Method</p>   
+            </td>
+            <td style="text-align: center;">
+            <p align="center">기능</p>
+            </td>
+            <td colspan="1" style="text-align: center;">
+            <p align="center">요청 파라메터 리스트</p>
+            </td>
+            <td colspan="1" style="text-align: center;">
+            <p align="center">응답 키 리스트</p>
+            </td>
+            <td colspan="1" style="text-align: center;">
+            <p align="center">인증 필요 여부</p>
+            </td>
+            <td colspan="1" style="text-align: center;">
+            <p align="center">에러목록</p>
+            </td>
+            
+            <td colspan="1" style="text-align: center;">
+            <p align="center">비고</p>
+            </td>
+            
+            </tr>`;
+            apiSpec.forEach((obj) => {
+                const item = obj.item;
+                htmlTable += `<tr>`;
+                if (!item) {
+                    htmlTable += `<td colspan="1" style="text-align: center;">${cnt++}</td>`   //번호
+                    htmlTable += `<td colspan="1" style="text-align: center;">unknown</td>`   //종류
+                    htmlTable += `<td colspan="1" style="text-align: center;">${obj.path}</td>`  //명칭
+                }
+                else if (item.hide) {
+                }
+                else if (!item.type) {
+                    htmlTable += `<td colspan="1" style="text-align: center;">${cnt++}</td>`   //번호
+                    htmlTable += `<td colspan="1" style="text-align: center;">unknown</td>`   //종류
+                    htmlTable += `<td colspan="1" style="text-align: center;">${obj.path}</td>`  //명칭
+                }
+                else {
+                    apiCount++;
+                    htmlTable += `<td colspan="1" style="text-align: center;">${cnt++}</td>`   //번호
+                    htmlTable += `<td colspan="1" style="text-align: center;">${item.type}</td>`   //종류
+                    htmlTable += `<td colspan="1" style="text-align: center;">${item.name}</td>`  //명칭
+                    htmlTable += `<td colspan="1" style="text-align: center;">${item.uri}</td>`  //api_uri
+                    htmlTable += `<td colspan="1" style="text-align: center;">${item.method}</td>` //method
+                    htmlTable += `<td colspan="1" style="text-align: left;">${item.desc}</td>`//기능
+
+                    htmlTable += `<td>` //요청 파라메터 리스트
+                    htmlTable += `<ul>`
+                    for (var property in item.parameters) {
+                        const obj = item.parameters[property];
+                        //minmax
+                        let minMax = "";
+                        if (obj.min != undefined && obj.max != undefined) {
+                            minMax = `(${obj.min}~${obj.max}${obj.type.toLowerCase() == "string" ? "글자" : ""})`;
+                        }
+                        else if (obj.min != undefined) {
+                            minMax = `(${obj.min}~${obj.type.toLowerCase() == "string" ? "글자" : ""})`;
+                        }
+                        else if (obj.max != undefined) {
+                            minMax = `(~${obj.max}${obj.type.toLowerCase() == "string" ? "글자" : ""})`;
+                        }
+                        htmlTable += `<li style="text-align: left;">${property}[${obj.type}]${!obj.req ? "(Optional)" : ""}:${obj.desc}${minMax == "" ? "" : minMax}</li>`
+                        if (obj.sub) {
+
+
+                            htmlTable += `<ul>`
+                            for (var prop in obj.sub) {
+                                const obj2 = obj.sub[prop];
+                                htmlTable += `<li style="text-align: left;">${prop}[${obj2.type}](${!obj2.req ? "Optional" : ""}):${obj2.desc}</li>`
+                            }
+                            htmlTable += `</ul>`
+                        }
+                    }
+                    htmlTable += `</ul>`
+                    htmlTable += `</td>`
+                    htmlTable += `<td>`//응답 파라메터 리스트
+                    htmlTable += `<ul>`
+
+                    for (var property in item.responses) {
+                        const obj = item.responses[property];
+                        htmlTable += `<li style="text-align: left;">${property}[${obj.type}]:${obj.desc}</li>`
+
+                        if (obj.sub) {
+                            htmlTable += `<ul>`
+                            for (var prop in obj.sub) {
+                                const obj2 = obj.sub[prop];
+                                htmlTable += `<li style="text-align: left;">${prop}[${obj2.type}]${obj2.searchable ? "(Searchable)" : ""}:${obj2.desc}</li>`
+                            }
+                            htmlTable += `</ul >`
+                        }
+                    }
+                    htmlTable += `</ul >`
+                    htmlTable += `</td>`
+
+                    htmlTable += `<td colspan="1" style="text-align: center;">`
+                    htmlTable += item.noAuth ? `<span style="color: rgb(255,0,0);"><b>False</b></span>` : `<span ><b>True</b></span>`;
+                    htmlTable += `</td>`
+
+                    htmlTable += `<td colspan="1" style="text-align: center;">`
+                    htmlTable += `<ul>`
+                    if (item && item.errors) {
+                        for (var property in item.errors) {
+                            const obj = item.errors[property];
+
+                            apiName.push(item.name)
+                            errorTitles.push(property);
+                            errorValues.push(obj.reason);
+
+                            htmlTable += `<li style="text-align: left;">${property}(${obj.status_code}):${obj.reason}</li>`
+
+                        }
+                    }
+                    htmlTable += `</ul>`
+                    htmlTable += `</td>`
+
+
+                    htmlTable += `<td colspan="1" style="text-align: center;">`
+                    if (item && item.comment) {
+
+                        htmlTable += `${item.comment}`;
+                    }
+                    htmlTable += `</td>`
+
+
+
+                }
+                htmlTable += `</tr>`;
+            })
+            htmlTable += `</tbody>`
+            htmlTable += `</table>`
+        }
+        if (cnt > 0) {
+            html += htmlTable;
+        }
+    }
+
+    const exportBlock = createPostmanImport(apiSpecList, title, stage, version, host);
+    //export
+    html += `<br></br>`;
+    html += `<p>ErrorCode 목록</p>`;
+
+    html += `<table>`;
+    html += `<tbody>`;
+    html += `<tr>`;
+
+
+    html += `<th>`;
+
+    html += "{";
+
+    html += `<br></br>`;
+
+    for (let i = 0; i < errorTitles.length; i++) {
+
+
+        html += `"${apiName[i]}-${errorTitles[i]}":"${errorValues[i]}",<br></br>`
+    }
+
+    html += "}"
+    html += `</th>`;
+    html += `</tr>`;
+    html += `</tbody>`;
+    html += `</table>`;
+
+    //--
+
+    html += `<br></br>`;
+    html += `<p>postman import json</p>`;
+
+    html += `<table>`;
+    html += `<tbody>`;
+    html += `<tr>`;
+
+
+    html += `<th>`;
+
+    html += exportBlock;
+    html += `</th>`;
+    html += `</tr>`;
+    html += `</tbody>`;
+    html += `</table>`;
+
+
+    if (confluenceSpaceName) {
+
+        //getpageVersion
+        const auth = Base64.encode("spark@twinny.co.kr" + ":" + "VQUqEYXASjJOOP9xuIzl7EF5");
+        const confData = await axios({
+            method: 'GET',
+            url: "https://twinny.atlassian.net/wiki/rest/api/content/" + confluencePageId,
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Basic " + Base64.encode(`${confluenceUserId}:${confluencePassword}`)
+            },
+        })
+        const versionNumber = confData.data.version.number
+        try {
+            var jsondata = {
+                "id": parseInt(confluencePageId),
+                "type": "page",
+                "title": `${title}-${stage}(${moment().tz("Asia/Seoul").format("MM-DD")})`,
+                "ancestors": [{ "id": parseInt(ancestorsPageId) }],
+                "space": { "key": confluenceSpaceName },
+                "version": { "number": versionNumber + 1 },
+                "body": { "storage": { "value": html, "representation": "storage" } }
+            };
+            const post = await axios({
+                method: 'PUT',
+                url: "https://twinny.atlassian.net/wiki/rest/api/content/" + confluencePageId,
+                data: JSON.stringify(jsondata),
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": "Basic " + Base64.encode(`${confluenceUserId}:${confluencePassword}`)
+                },
+            })
+            console.log("Basic " + Base64.encode(`${confluenceUserId}:${confluencePassword}`));
+            console.log(post);
+        }
+        catch (e) {
+            console.log(e.response.data.message);
+        }
+    }
+}
+
+
+
+module.exports.handleCommit = handleCommit;
 module.exports.generateServerlessFunction = generateServerlessFunction;
 module.exports.generateGraphQL = generateGraphQL;
 
