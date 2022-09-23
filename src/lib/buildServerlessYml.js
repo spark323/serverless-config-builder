ebe4ed04d5e7eedf9798b57e27a8c96cc7a6e21c
spark <innocentchris@gmail.com>
2022-09-23 19:18:01 +0900

    cmt

--- src/lib/buildServerlessYml.js
+++ src/lib/buildServerlessYml.js
@@ -1,10 +1,8 @@
 #!/usr/bin/env node
 const yargs = require("yargs");
-const { exec } = require('child_process');
 var builder = require("./builder");
 require('dotenv').config()
 const { hideBin } = require('yargs/helpers');
-const { makeConsoleLogger } = require("@notionhq/client/build/src/logging");
 const argv = yargs(hideBin(process.argv)).argv
 
 let templateFile = (argv.t) ? argv.t : "serverless_template.yml";
@@ -13,7 +11,7 @@ if (argv.n) {
     builder.uploadToNotion(argv.n);
 }
 else {
-    (argv.x) ? builder.generateExportFile() : builder.generateServerlessFunction(`./${templateFile}`,process.env.STAGE?process.env.STAGE:argv.stage,process.env.VER?process.env.VER:argv.ver);
+    (argv.x) ? builder.generateExportFile() : builder.generateServerlessFunction(`./${templateFile}`, process.env.STAGE ? process.env.STAGE : argv.stage, process.env.VER ? process.env.VER : argv.ver);
 }
 
 
