5fc5ef453f4193360051ae5f6c9489d9aa31c502
spark <innocentchris@gmail.com>
2022-12-23 21:25:29 +0900

    cmt

--- src/lib/buildServerlessYml.js
+++ src/lib/buildServerlessYml.js
@@ -7,16 +7,14 @@ const argv = yargs(hideBin(process.argv)).argv
 
 let templateFile = (argv.t) ? argv.t : "serverless_template.yml";
 
-try
-{
-if (argv.n) {
-    builder.uploadToNotion(argv.n, process.env.STAGE, process.env.VER);
-}
-else {
-    (argv.x) ? builder.generateExportFile() : builder.generateServerlessFunction(`./${templateFile}`, argv.stage ? argv.stage : process.env.STAGE, argv.ver ? argv.ver : process.env.VER);
-}
-}catch(e)
-{
+try {
+    if (argv.n) {
+        builder.uploadToNotion(process.env.NOTION_SECRET ? process.env.NOTION_SECRET : argv.n, process.env.STAGE, process.env.VER);
+    }
+    else {
+        (argv.x) ? builder.generateExportFile() : builder.generateServerlessFunction(`./${templateFile}`, argv.stage ? argv.stage : process.env.STAGE, argv.ver ? argv.ver : process.env.VER);
+    }
+} catch (e) {
     console.error(e);
 }
 
